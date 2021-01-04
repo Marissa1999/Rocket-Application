8190ffff34dd28aa813219a300a572731ed773e4
Marissa Gonçalves <marissagoncalves13@gmail.com>
2021-01-04 17:42:42 -0500

    Initial Commit.

---
+++ Rocket.java
@@ -0,0 +1,32 @@
+//Programmer: Marissa Gonçalves
+//Date: August 25, 2017
+//Purpose: This program must use escape sequences to create a rocket ship.
+
+public class Rocket
+
+{
+  public static void main (String[]args)
+
+   {
+         //print a message by using escape sequences as follows;
+         System.out.println ("   /\\");
+         System.out.println ("  //\\\\");
+         System.out.println (" /\\/\\/\\");
+         System.out.println ("+------+");
+         System.out.println ("|      |");
+         System.out.println ("|  GO  |");
+         System.out.println ("+------+");
+         System.out.println ("|      |");
+         System.out.println ("| Java |");
+         System.out.println ("+------+");
+         System.out.println ("|      |");
+         System.out.println ("|  GO  |");
+         System.out.println ("+------+");
+         System.out.println ("   /\\");
+         System.out.println ("  //\\\\");
+         System.out.println (" /\\/\\/\\");
+         System.out.println ();
+
+    }
+
+}
